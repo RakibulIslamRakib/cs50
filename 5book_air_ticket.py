05d7ef09f90220178ad8eae5283c8c1270994db5
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2020-04-15 04:12:52 -0700

    Update 5book_air_ticket.py

--- 5book_air_ticket.py
+++ 5book_air_ticket.py
@@ -27,6 +27,21 @@ def book():
     return render_template("success.html")
 
 
+@app.route('/flights')
+def flights():
+    flights = db.execute("Select * from flight").fetchall()
+    return render_template("flights.html",flights=flights)
+
+
+@app.route('/flights/<int:flight_id>')
+def flight(flight_id):
+    flight = db.execute("Select * from flight where id =:id",{"id":flight_id}).fetchone()
+    if flight is None:
+        return render_template("error.html",messes="NO such flight")
+    passengers = db.execute("Select * from passengers where flight_id =:flight_id",{"flight_id":flight_id}).fetchall()
+    return render_template("flight.html",passengers=passengers,flight=flight)
+
+
 if __name__ == '__main__':
     app.run()
 
@@ -108,5 +123,54 @@ success.html
     <h1>
     you have successfully book your flight
     </h1>
+{%endblock%}
+'''
+
+
+'''
+flights.html
+{% extends "laout.html" %}
+{%block heading%}<title>Flights</title>{%endblock%}
+{%block body%}
+    <h1>
+    All the flights are:<br>
+    </h1>
+    <ul>
+    {% for flight in flights %}
+        <li>
+        <a href="{{ url_for('flight',flight_id = flight.id) }}">from {{flight.region }} to {{ flight.destination }}</a>
+        </li>
+    {% endfor %}
+    </ul>
+{%endblock%}
+
+'''
+
+
+'''
+flight.html
+{% extends "laout.html" %}
+{%block heading%}<title>Flight</title>{%endblock%}
+{%block body%}
+    <h1>
+    Flight deteils:<br>
+    </h1>
+    <ul>
+        <li><h3>Region: {{ flight.region }}</h3></li>
+        <li><h3>Destination: {{ flight.destination }}</h3></li>
+        <li><h3>Duretion: {{ flight.durretion }}</h3></li>
+    </ul>
+    <ul>
+    <h1>Passengers:</h1>
+    {% for passenger in passengers %}
+        <li>
+        <h3>{{passenger.name  }}</h3>
+        </li>
+    {% else%}
+        <li>
+        <h3>No passenger</h3>
+        </li>
+    {% endfor %}
+    </ul>
 {%endblock%}
 '''
