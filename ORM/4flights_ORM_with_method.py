fbcc1b6963b888c99a23de630541e3ba6f045338
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2020-04-27 22:37:05 -0700

    Create 4flights_ORM_with_method.py

---
+++ ORM/4flights_ORM_with_method.py
@@ -0,0 +1,96 @@
+#models.py:
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Flight(db.Model):
+    __tablename__ = "flights"
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+    def add_passenger(self, name):
+        p = Passenger(name=name, flight_id=self.id)
+        db.session.add(p)
+        db.session.commit()
+
+
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
+    
+#app.py
+from flask import Flask, render_template, request
+from models import *
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'rakib1602066',
+    'db': 'mydatabase',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+#app.config["SQLALCHEMY_DATABASE_URL"]="postgresql://postgres:rakib1602066@localhost/mydatabase"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
+@app.route("/")
+def index():
+    flights = Flight.query.all()
+    return render_template("index.html", flights=flights)
+
+
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight."""
+
+    # Get form information.
+    name = request.form.get("name")
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number.")
+
+    # Make sure the flight exists.
+    flight = Flight.query.get(flight_id)
+    if not flight:
+        return render_template("error.html", message="No such flight with that id.")
+
+    # Add passenger.
+    flight.add_passenger(name)
+    return render_template("success.html")
+
+
+@app.route("/flights")
+def flights():
+    """List all flights."""
+    flights = Flight.query.all()
+    return render_template("flights.html", flights=flights)
+
+
+@app.route("/flights/<int:flight_id>")
+def flight(flight_id):
+    """List details about a single flight."""
+
+    # Make sure flight exists.
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return render_template("error.html", message="No such flight.")
+
+    # Get all passengers.
+    passengers = Passenger.query.filter_by(flight_id=flight_id).all()
+    return render_template("flight.html", flight=flight, passengers=passengers)
+    
+    
+    """
+    html files are same"""
+
